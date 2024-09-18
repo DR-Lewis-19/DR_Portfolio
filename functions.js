b36570e32a145ea0fe315cbb0142f40600aff820
DR-Lewis-19 <damiquelewis43@gmail.com>
2024-09-18 13:34:47 +0200

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,7 @@
+// Add event listener to button
+document.querySelector('button').addEventListener('click', function() {
+    // Scroll to projects section
+    document.querySelector('#projects').scrollIntoView({ behavior: 'smooth' });
+  });
+
+  
